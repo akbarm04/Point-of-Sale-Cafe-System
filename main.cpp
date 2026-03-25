fc69e40d62e0e679817789451b16e3ad1168e072
fatahidayat <fatahnurhidayat61@gmail.com>
2026-03-25 22:38:13 +0700

    Final: struktur menu dan sistem kasir

--- main.cpp
+++ main.cpp
@@ -1,87 +1,82 @@
 #include <iostream>
-#include <iomanip>
+#include <string>
 using namespace std;
 
-// === BAGIAN 1: STRUCT & ARRAY MENU ===
-// Oleh: Fatah (UI Designer & Master Data)
-
+//STRUCT
 struct Menu {
     int id;
     string nama;
+    string kategori;
     int harga;
 };
 
-// Array Menu (Database Menu Cafe)
+//ARRAY (Database Menu Cafe)
 Menu daftarMenu[10] = {
-    {1, "Americano", 15000},
-    {2, "Cappuccino", 18000},
-    {3, "Latte", 20000},
-    {4, "Matcha", 22000},
-    {5, "Chocolate", 21000},
-    {6, "Croissant", 12000},
-    {7, "Donat", 10000},
-    {8, "Sandwich", 17000},
-    {9, "French Fries", 15000},
-    {10, "Cheesecake", 25000}
+    {1, "Americano", "Minuman", 15000},
+    {2, "Cappuccino", "Minuman", 18000},
+    {3, "Latte", "Minuman", 20000},
+    {4, "Matcha", "Minuman", 22000},
+    {5, "Chocolate", "Minuman", 21000},
+    {6, "Croissant", "Makanan", 12000},
+    {7, "Donat", "Makanan", 10000},
+    {8, "Sandwich", "Makanan", 17000},
+    {9, "French Fries", "Makanan", 15000},
+    {10, "Cheesecake", "Makanan", 25000}
 };
 
-// Fungsi untuk menampilkan menu
+//FUNGSI TAMPIL MENU
 void tampilkanMenu(Menu menu[], int size) {
-    cout << "\n=====================================\n";
-    cout << "           MENU CAFE\n";
-    cout << "=====================================\n";
-    cout << left << setw(5) << "ID"
-         << setw(20) << "Nama"
-         << setw(10) << "Harga" << endl;
-    cout << "-------------------------------------\n";
+    cout << "===== MENU CAFE =====" << endl;
 
     for(int i = 0; i < size; i++) {
-        cout << left << setw(5) << menu[i].id
-             << setw(20) << menu[i].nama
-             << "Rp" << menu[i].harga << endl;
+        cout << menu[i].id << ". "
+             << menu[i].nama << " - "
+             << menu[i].kategori << " - Rp"
+             << menu[i].harga << endl;
     }
-
-    cout << "=====================================\n";
 }
 
-// === MAIN MENU (CLI) ===
+//MAIN (SISTEM KASIR)
 int main() {
     int pilihan;
 
-    do {
-        cout << "\n=====================================\n";
-        cout << "     SISTEM POINT OF SALE CAFE\n";
-        cout << "=====================================\n";
-        cout << "1. Lihat Menu\n";
-        cout << "2. Tambah ke Keranjang\n";
-        cout << "3. Lihat Keranjang\n";
-        cout << "4. Checkout\n";
-        cout << "5. Keluar\n";
-        cout << "-------------------------------------\n";
-        cout << "Pilih menu: ";
-        cin >> pilihan;
+    cout << "===========================" << endl;
+    cout << "SISTEM KASIR & ANTREAN CAFE" << endl;
+    cout << "===========================" << endl;
 
-        switch(pilihan) {
-            case 1:
-                tampilkanMenu(daftarMenu, 10);
-                break;
-            case 2:
-                cout << "\n[INFO] Fitur tambah ke keranjang (Anggota 2)\n";
-                break;
-            case 3:
-                cout << "\n[INFO] Fitur lihat keranjang (Anggota 2 & 3)\n";
-                break;
-            case 4:
-                cout << "\n[INFO] Fitur checkout (Anggota 5)\n";
-                break;
-            case 5:
-                cout << "\nTerima kasih telah menggunakan sistem!\n";
-                break;
-            default:
-                cout << "\nPilihan tidak valid!\n";
-        }
+    cout << "1. Lihat menu dan pesan" << endl;
+    cout << "2. Lihat keranjang pelanggan" << endl;
+    cout << "3. Checkout dan bayar" << endl;
+    cout << "4. Lihat antrean dapur" << endl;
+    cout << "5. Keluar" << endl;
+    cout << "Pilih menu: ";
+    cin >> pilihan;
 
-    } while(pilihan != 5);
+    switch(pilihan) {
+        case 1:
+            tampilkanMenu(daftarMenu, 10);
+            cout << "[INFO] Tambah ke keranjang (rifa)" << endl;
+            break;
+
+        case 2:
+            cout << "[INFO] Lihat keranjang (rifa & akbar)" << endl;
+            break;
+
+        case 3:
+            cout << "[INFO] Checkout & pembayaran (iky)" << endl;
+            break;
+
+        case 4:
+            cout << "[INFO] Antrean dapur (danish)" << endl;
+            break;
+
+        case 5:
+            cout << "Terima kasih" << endl;
+            break;
+
+        default:
+            cout << "Pilihan tidak valid" << endl;
+    }
 
     return 0;
 }
